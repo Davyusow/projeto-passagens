4e74dac0d129b1db7ae0594b460b081921d7e6bf
Dayvson Farias <davyusow@localhost.localdomain>
2025-07-28 15:19:13 -0300

    setup do javafx

--- src/main/java/projeto/main.java
+++ src/main/java/projeto/main.java
@@ -1,9 +1,23 @@
 package projeto;
 
-public class main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage janela) throws Exception {
+        Parent raiz = FXMLLoader.load(getClass().getResource("tela.fxml"));
+        janela.setScene(new Scene(raiz));
+        janela.setTitle("Menu inicial");
+        janela.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        launch(args);
     }
 
 }
