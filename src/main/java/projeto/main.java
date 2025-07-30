a1213cd10b54cf9d6ddfe438d5cb49fcd9a03512
JosueC006 <josue2014cs@gmail.com>
2025-07-30 20:59:48 -0300

    Adições iniciais [esqueci de criar mminha branche]

--- src/main/java/projeto/main.java
+++ src/main/java/projeto/main.java
@@ -10,7 +10,7 @@ public class main extends Application {
 
     @Override
     public void start(Stage janela) throws Exception {
-        Parent raiz = FXMLLoader.load(getClass().getResource("tela.fxml"));
+        Parent raiz = FXMLLoader.load(getClass().getResource("telaEntrada.fxml"));
         janela.setScene(new Scene(raiz));
         janela.setTitle("Menu inicial");
         janela.show();
