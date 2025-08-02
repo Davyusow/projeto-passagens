2ce96c01ef820485798d2cc5f014ffd588abfb00
Valadyne <caio.marinho@ufrpe.br>
2025-08-02 15:56:51 -0300

    Implementação dos DAOs
    - auto explicativo

--- src/main/java/projeto/DAO/ReservaDAOImpl.java
+++ src/main/java/projeto/DAO/ReservaDAOImpl.java
@@ -1,5 +1,36 @@
 package projeto.DAO;
 
-public class ReservaDAOImpl {
+import java.util.List;
 
+import projeto.models.Reserva;
+import projeto.repos.RepositorioReservas;
+
+public class ReservaDAOImpl implements ObjectDAO<Reserva> {
+	
+	private final RepositorioReservas repositorio = RepositorioReservas.getInstance();
+
+	@Override
+	public void criar(Reserva reserva) {
+		repositorio.criarReserva(reserva);
+	}
+	
+	@Override
+	public Reserva procurar(int id) {
+		return repositorio.procurarReserva(id);
+	}
+	
+	@Override
+	public boolean editar(Reserva reserva) {
+		return repositorio.editarReserva(reserva);
+	}
+	
+	@Override
+	public boolean remover(Reserva reserva) {
+		return repositorio.removerReserva(reserva);
+	}
+	
+	@Override
+	public List<Reserva> findAll() {
+		return repositorio.getReservas();
+	}
 }
